a9972fff85920852affd764d6bcc786d8abd127b
Noah Graells <noah.graells@he-arc.ch>
2019-02-20 11:15:42 +0100

    Register models for admin

--- sandwish/sandwish_app/admin.py
+++ sandwish/sandwish_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Wishlist, Gift, Contribution
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Wishlist)
+admin.site.register(Gift)
+admin.site.register(Contribution)
