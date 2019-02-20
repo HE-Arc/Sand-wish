6a27f61d0112df4233da03be23c4fdc74526d8af
Guillaume Chacun <guillaume.chacun@he-arc.ch>
2019-02-20 11:36:44 +0100

    Create basic routes

--- sandwish/sandwish_app/views.py
+++ sandwish/sandwish_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.views import generic, View
+from django.urls import reverse_lazy
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'sandwish_app/index.html', context)
