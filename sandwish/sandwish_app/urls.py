8c8cf1b6bc2480baf62bfb3a75ae965154904a72
Guillaume Chacun <guillaume.chacun@he-arc.ch>
2019-02-21 15:01:42 +0100

    Display user's wishlists

--- sandwish/sandwish_app/urls.py
+++ sandwish/sandwish_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, re_path
+from django.contrib import admin
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("signup/", views.signup, name="signup"),
-    re_path(r"^(?P<slug>[\w.@+-]+)/$", views.ProfileView.as_view(), name="profile"),
+    path("admin/", admin.site.urls),
+    path("<str:slug>/", views.ProfileView.as_view(), name="profile"),
 ]
--- sandwish/sandwish_app/views.py
+++ sandwish/sandwish_app/views.py
@@ -5,8 +5,7 @@ from django.urls import reverse_lazy
 
 # for signup
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.models import User
-# from .models import User
+from .models import User, Wishlist
 from sandwish_app.forms import SignUpForm
 
 
@@ -29,13 +28,14 @@ def signup(request):
     return render(request, "registration/signup.html", {"form": form})
 
 class ProfileView(generic.DetailView):
-    model = User
+    model=User
     slug_field = "username"
     template_name = "sandwish_app/profile.html"
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profiles_user = context["object"]
 
-    #     # context[]
+        context["wishlists"] = Wishlist.objects.filter(user_id=profiles_user.id)
 
-    #     return context
+        return context
