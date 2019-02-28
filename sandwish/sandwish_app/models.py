712952c11d48c515b6fb1a6c8ab674dc3de0e51a
Noah Graells <noah.graells@he-arc.ch>
2019-02-28 10:25:21 +0100

    Fix gift model, user to wishlist

--- sandwish/sandwish_app/models.py
+++ sandwish/sandwish_app/models.py
@@ -21,7 +21,7 @@ class Gift(models.Model):
     link = models.CharField(max_length=256)
     validated = models.BooleanField(default=False)
 
-    user = models.ForeignKey('Wishlist', on_delete = models.CASCADE)
+    wishlist = models.ForeignKey('Wishlist', on_delete = models.CASCADE)
 
     def __str__(self):
         return self.name
