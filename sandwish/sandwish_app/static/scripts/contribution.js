2f8f5a9e5303303809c4ed084882af811836e005
fabien.vorpe <fabien.vorpe@he-arc.ch>
2019-04-01 13:39:18 +0200

    progress bar for contribution now adpats in ajax aswell

--- sandwish/sandwish_app/static/scripts/contribution.js
+++ sandwish/sandwish_app/static/scripts/contribution.js
@@ -3,10 +3,10 @@
  * @param {int} giftId : gift's id
  * @param {String} crsftoken : contribution form's crsf token
  */
-function contributionSubmit(giftId, csrf_token) {
+function contributionSubmit(giftId, giftPrice, csrf_token) {
     if (event)
         event.preventDefault();
-    create_contribution(giftId, csrf_token);
+    create_contribution(giftId, giftPrice, csrf_token);
 }
 
 /**
@@ -14,7 +14,7 @@ function contributionSubmit(giftId, csrf_token) {
  * @param {int} giftId : gift's id
  * @param {String} crsftoken : contribution form's crsf token
  */
-function create_contribution(giftId, csrf_token) {
+function create_contribution(giftId, giftPrice, csrf_token) {
     valueFieldName = "#contribution" + giftId;
 
     $.ajax({
@@ -31,10 +31,16 @@ function create_contribution(giftId, csrf_token) {
             // console.log(json); // log the returned json to the console
             if (json.result == "success"){
                 $("#full_contribution" + giftId).text(json.new_total_contribution);
+                var elem = $("#user-progress-bar" + giftId);
+                elem.html(json.value);
+                if (!isNaN(giftPrice)){
+                    let new_percent_value = json.value / giftPrice * 100;
+                    elem.css("width", new_percent_value+"%");
+                }
             }
             else if (json.result == "fail"){
                 $(valueFieldName).val(json.old_value);
-                $('#contribution-error' + giftId).text(json.error_message);
+                $("#contribution-error" + giftId).text(json.error_message);
             }
         },
 
