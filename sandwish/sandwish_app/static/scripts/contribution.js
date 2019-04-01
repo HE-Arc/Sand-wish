e297162c3fd713c476dcd46d8538c517eedcea7b
fabien.vorpe <fabien.vorpe@he-arc.ch>
2019-04-01 11:26:10 +0200

    comment problem with the merge

--- sandwish/sandwish_app/static/scripts/contribution.js
+++ sandwish/sandwish_app/static/scripts/contribution.js
@@ -23,21 +23,23 @@ function create_contribution(giftId, csrf_token) {
         data : {
             "value": $(valueFieldName).val(),
             "giftId": giftId,
-            "csrfmiddlewaretoken": csrf_token
+            'csrfmiddlewaretoken': csrf_token
         },
 
         // handle a successful response
-        success: function(json) {
-            if (json.result == "success")
+        success : function(json) {
+            // console.log(json); // log the returned json to the console
+            if (json.result == "success"){
                 $("#full_contribution" + giftId).text(json.new_total_contribution);
-            else if (json.result == "fail")
+            }
+            else if (json.result == "fail"){
                 $(valueFieldName).val(json.old_value);
                 $('#contribution-error' + giftId).text(json.error_message);
             }
         },
 
         // handle a non-successful response
-        error : (xhr, _, __) => {
+        error : function(xhr,errmsg,err) {
             console.log("fail");
             console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
         }
